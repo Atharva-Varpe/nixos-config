dccba4ca256194699026c98bd10961ff85b12643
Atharva <atharva.varpe2019@gmail.com>
2026-07-25 21:50:13 +0530

    chore: remove unused nvim-config package

--- pkgs/overlay.nix
+++ pkgs/overlay.nix
@@ -2,7 +2,6 @@
 self: super: {
   antigravity-cli = self.callPackage ./antigravity-cli { };
   command-code = self.callPackage ./command-code { };
-  nvim-config = self.callPackage ./nvim-config { };
   zen-browser = zen-browser.packages.${system}.zen-browser;
   zen-browser-unwrapped = zen-browser.packages.${system}.zen-browser-unwrapped;
   unstable = import nixpkgs-unstable {
